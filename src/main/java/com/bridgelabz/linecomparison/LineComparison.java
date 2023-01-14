e6c3bdc24a589704c687b4aae9db5378ed2b67c8
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-14 12:37:34 +0530

    Added:Length of a line

--- src/main/java/com/bridgelabz/linecomparison/LineComparison.java
+++ src/main/java/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,7 +1,20 @@
 package com.bridgelabz.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Problem");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter coordinate of x1");
+        int x1 = scanner.nextInt();
+        System.out.println("Enter coordinate of y1");
+        int y1 = scanner.nextInt();
+        System.out.println("Enter coordinate of x2");
+        int x2 = scanner.nextInt();
+        System.out.println("Enter coordinate of y2");
+        int y2 = scanner.nextInt();
+        double len = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("The length of line drawn by points ( " + x1 +"," + y1 + " ) and ( " + x2 + "," + y2 + " ) is: " + len);
     }
 }
