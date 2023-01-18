2e908577c40ff1a02a30b40fc8befa5e638712c8
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-18 15:43:45 +0530

    Refactored:Comparing length of two lines

--- src/main/java/com/bridgelabz/linecomparison/LineComparison.java
+++ src/main/java/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,6 +3,18 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparison {
+    double calculateLength(int x1, int y1, int x2, int y2) {
+        return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    }
+    void compareLength(double length1, double length2) {
+        if(length1==length2)
+            System.out.println("Lines are equal");
+        else
+        if (length1 > length2)
+            System.out.println(" Length of Line1 is greater than Line 2");
+        else
+            System.out.println(" Length of Line1 is less than Line 2");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Problem");
         Scanner scanner = new Scanner(System.in);
@@ -22,14 +34,9 @@ public class LineComparison {
         int x4 = scanner.nextInt();
         System.out.println("Enter coordinate of y2");
         int y4 = scanner.nextInt();
-        double len1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-        double len2 = Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
-        if(len1==len2)
-            System.out.println("Lines are equal");
-        else
-        if (len1 > len2)
-            System.out.println(" Length of Line1 is greater than Line 2");
-        else
-            System.out.println(" Length of Line1 is less than Line 2");
+        LineComparison linecomparison = new LineComparison();
+        double length1 = linecomparison.calculateLength(x1, y1, x2, y2);
+        double length2 = linecomparison.calculateLength(x3, y3, x4, y4);
+        linecomparison.compareLength(length1,length2);
     }
 }
